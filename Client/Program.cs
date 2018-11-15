b49cca92c1b5c38a96b3c1164fe7f8f67a084cca
Stefan Stipanovic <s_stefan@outlook.com>
2018-11-15 19:51:46 +0100

    Added menu for client

--- Client/Program.cs
+++ Client/Program.cs
@@ -18,6 +18,7 @@ namespace Client
 
             string address = "net.tcp://localhost:9999/WCFService";
 
+            var rand = new Random();
             using (WCFClient proxy = new WCFClient(binding, new EndpointAddress(new Uri(address))))
             {
                 /// If client was started via ClientTest.
@@ -25,7 +26,6 @@ namespace Client
                 {
                     while (true)
                     {
-                        var rand = new Random();
                         System.Threading.Thread.Sleep(rand.Next(500, 1000));
                         proxy.GetBill(rand.Next(10000000, 10000017));
                     }
@@ -33,24 +33,12 @@ namespace Client
                 /// If client was started normally.
                 else
                 {
-                    //proxy.AddDB(121, "Petar", 923.12);
-                    //proxy.AddDB(122, "Petar", 2123.12);
-                    //proxy.AddDB(123, "Petar", 523.12);
-                    //proxy.AddDB(124, "Petar", 1233.12);
-                    //proxy.AddDB(125, "Petar", 1232.12);
-                    //proxy.AddDB(126, "Petar", 1231.12);
-                    //proxy.AddDB(127, "Petar", 1123.12);
-                    //proxy.AddDB(128, "Petar", 13.12);
-                    //proxy.AddDB(129, "Petar", 23.2);
-                    //proxy.AddDB(131, "Petar", 123.12);
-                    //proxy.AddDB(143, "Petar", 12.12);
-                    //proxy.AddDB(153, "Petar", 23.1);
-                    //proxy.AddDB(163, "Petar", 423.12);
-                    //proxy.ModifyID(321, 123);
-                    //proxy.ModifyReading(431.12, 321);
-                    //proxy.DeleteEntityDB(123);
-                    //proxy.DeleteDB();
-                    //proxy.GetBill(123);
+                    //for(int i = 0; i < 100; ++i)
+                    //{
+                    //    proxy.AddDB(10000000 + i, "pera peric", rand.NextDouble() * rand.Next(100, 10000));
+                    //}                    
+
+                    Menu(proxy);
                 }
             }
 
@@ -58,9 +46,76 @@ namespace Client
         }
 
         // TODO:
-        public void Menu()
+        public static void Menu(WCFClient proxy)
         {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. GetBill.");
+                Console.WriteLine("2. ModifyID.");
+                Console.WriteLine("3. ModifyReading.");
+                Console.WriteLine("4. DeleteEntity.");
+                Console.WriteLine("5. DeleteDB.");
+                Console.WriteLine("6. AddDB.");
+                Console.WriteLine("-----------------------");
+                Console.Write("Enter your choice: ");
 
+                int choice = 0;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                    continue;
+                int id = 0, newID = 0;
+                double newReading = 0;
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter ID: ");
+                        if(int.TryParse(Console.ReadLine(), out id))
+                            proxy.GetBill(id);
+                        break;
+                    case 2:
+                        Console.Write("Enter ID: ");
+                        if (int.TryParse(Console.ReadLine(), out id))
+                        {
+                            Console.Write("Enter new id: ");
+                            if (int.TryParse(Console.ReadLine(), out newID))
+                                proxy.ModifyID(newID, id);
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Enter ID: ");
+                        if (int.TryParse(Console.ReadLine(), out id))
+                        {
+                            Console.Write("Enter new reading: ");
+                            if (double.TryParse(Console.ReadLine(), out newReading))
+                                proxy.ModifyReading(newReading, id);
+                        }
+                        break;
+                    case 4:
+                        Console.Write("Enter ID: ");
+                        if(int.TryParse(Console.ReadLine(), out id))
+                            proxy.DeleteEntityDB(id);
+                        break;
+                    case 5:
+                        proxy.DeleteDB();
+                        break;
+                    case 6:
+                        Console.Write("Enter ID: ");
+                        if (int.TryParse(Console.ReadLine(), out id))
+                        {
+                            Console.Write("Enter full name: ");
+                            var name = Console.ReadLine();
+                            Console.Write("Enter reading: ");
+                            if (double.TryParse(Console.ReadLine(), out newReading))
+                                proxy.AddDB(id, name, newReading);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                Console.Write("Press <enter> continue...");
+                Console.ReadLine();
+            }
         }
     }
 }
